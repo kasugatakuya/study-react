d69d2e18f798f0093d03ad7858328f1588fd4833
kasugatakuya <rxsgw123@yahoo.co.jp>
2022-11-23 12:59:28 +0900

    ♻️ ヘッドライン部分をコンポーネント化

---
+++ components/Headline.tsx
@@ -0,0 +1,15 @@
+import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from 'react'
+import styles from '../styles/Home.module.css'
+
+export function Headline(props: { page: string }) {
+  return (
+    <div>
+      <h1 className={styles.title}>{props.page} page</h1>
+
+      <p className={styles.description}>
+        Get started by editing{' '}
+        <code className={styles.code}>pages/{props.page}.tsx</code>
+      </p>
+    </div>
+  )
+}
