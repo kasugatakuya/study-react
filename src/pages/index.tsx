e6782afc7339f38935acb78f87836cc035cea9b1
kasugatakuya <rxsgw123@yahoo.co.jp>
2022-12-18 14:03:23 +0900

    ⚡️ useCallbackとuseEffectの第二引数の説明

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,21 @@ import styles from '../styles/Home.module.css'
 import { Footer } from "../components/Footer"
 import { Main } from "../components/Main"
 import { Header } from '../components/Header'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import Link from 'next/link'
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-
-  const hendleClick = () => {
-    setCount(count => count + 1);
-    setCount(count => count + 1);
-  };
+  const hendleClick = useCallback (() => {
+    if (count < 10) {
+      setCount(count => count + 1);
+    }
+  }, [count]);
   useEffect(() => {
+    // マウント時
     document.body.style.backgroundColor = "lightblue";
+    // アンマウント時
     return () => {
       document.body.style.backgroundColor = "";
     }
