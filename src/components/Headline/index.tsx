dd9f5546a727221a9723ace4603a6b28c08a7705
kasugatakuya <rxsgw123@yahoo.co.jp>
2023-01-02 16:30:24 +0900

    ✨ コンポーネント間のStateのリフトアップ

--- src/components/Headline/index.tsx
+++ src/components/Headline/index.tsx
@@ -1,15 +1,17 @@
 import classes from './Headline.module.css'
 import { ReactNode } from 'react'
 
-export function Headline(props: { page: string, number: number, array: number[], obj: object, boolean: boolean,  children: ReactNode
+export function Headline(props: { page: string, number: number, array: number[], obj: object, handleReduce: any, boolean: boolean,  children: ReactNode
 }) {
   return (
     <div>
       <h1 className={classes.title}>{props.page} page</h1>
 
       <p className={classes.description}>
-        Get started by editing{props.children}
+        アイテム数は{props.children}個です。
       </p>
+
+      <button onClick={props.handleReduce}>減らす</button>
     </div>
   )
 }
