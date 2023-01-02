97585382722cd45fd349722becc66a3f642d10c1
kasugatakuya <rxsgw123@yahoo.co.jp>
2023-01-02 12:41:28 +0900

    ♻️ 状態を変更するときにprevをつけて名前を分かりやすくした。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,20 +10,13 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState('');
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState<string[]>([]);
 
   const hendleClick = useCallback (() => {
     if (count < 10) {
-      setCount(count => count + 1);
+      setCount(prevCount => prevCount + 1);
     }
   }, [count]);
-  useEffect(() => {
-    // マウント時
-    document.body.style.backgroundColor = "lightblue";
-    // アンマウント時
-    return () => {
-      document.body.style.backgroundColor = "";
-    }
-  }, [])
 
   const handleChange = useCallback((e: any) => {
     if (e.target.value.length > 5) {
@@ -33,9 +26,28 @@ export default function Home() {
     setText(e.target.value.trim())
   }, []);
 
-  const handledisplay = useCallback((e: any) => {
-    setIsShow((isShow) => !isShow);
+  const handleDisplay = useCallback((e: any) => {
+    setIsShow((prevIsShow) => !prevIsShow);
   }, []);
+
+  const handleAdd = useCallback((e: any) => {
+    setArray((prevArray: string[]) => {
+      // if (prevArray.some((item) => item === text)) {
+      //   alert("同じ要素が既に存在します");
+      //   return;
+      // }
+      return [...prevArray, text];
+    })
+  }, [text]);
+
+  useEffect(() => {
+    // マウント時
+    document.body.style.backgroundColor = "lightblue";
+    // アンマウント時
+    return () => {
+      document.body.style.backgroundColor = "";
+    }
+  }, [])
 
   return (
     <div className={styles.container}>
@@ -52,9 +64,17 @@ export default function Home() {
       <button onClick={hendleClick} >
         ボタン
       </button>
-      <button onClick={handledisplay} >
+      <button onClick={handleDisplay} >
         {isShow ? "非表示" : "表示"}
       </button>
+      <button onClick={handleAdd} >
+        追加
+      </button>
+      <ul>
+        {array.map((item, index) => {
+          return <li key={index}>{item}</li>
+        })}
+      </ul>
       <Main page="index"/>
       <Footer/>
     </div>
