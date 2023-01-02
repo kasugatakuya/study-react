309cc3fae982bd1836802e673f26e80e46713490
kasugatakuya <rxsgw123@yahoo.co.jp>
2023-01-02 16:53:48 +0900

    ✨ useRouterを学びました

---
+++ src/hooks/useBgColor.tsx
@@ -0,0 +1,30 @@
+import { useRouter } from 'next/router';
+import { useEffect, useMemo } from 'react'
+
+export function useBgColor(): void {
+  const router = useRouter();
+
+  const bgColor = useMemo(() => {
+    switch (router.pathname) {
+      case "/": {
+        return "lightblue";
+      }
+      case "/about": {
+        return "beige";
+      }
+      default: {
+        return "";
+      }
+    }
+    // return router.pathname === "/" ? "lightblue" : "beige"
+  }, [router.pathname])
+
+  useEffect(() => {
+    // マウント時
+    document.body.style.backgroundColor = bgColor;
+    // アンマウント時
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, [bgColor]);
+}
